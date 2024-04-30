b21b47d7559b0f0b01f9c5c69501d7910da1e0c7
kimeunsu <kimes1405@gmail.com>
2024-04-30 23:45:14 +0900

    chat

--- ChatView/ChatView.js
+++ ChatView/ChatView.js
@@ -1,10 +1,10 @@
-import { FlatList, StyleSheet, SafeAreaView, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { FlatList, StyleSheet, SafeAreaView, Text, KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
 import TopBar from './TopBar';
 import Input from './Input';
 import Button from './Button';
 import Message from './Message';
 import BoxComponent from './BoxComponent';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function formatDate(date) {
   const year = date.getFullYear();
@@ -15,38 +15,75 @@ function formatDate(date) {
 }
 
 const ChatView = ({ navigation, name }) => {
-
+  StatusBar.setBarStyle('light-content');
   //채팅창 인사말
   const [messages, setMessages] = useState([
     { id: 'date', text: formatDate(new Date()), sender: 'date' },
     { id: '0', text: '안녕하세요! 저는 당신의 꿈을 분석하고, 꿈 속 상징들의 의미 파악을 통해 스스로의 감정을 파악할 수 있도록 돕는 가상 심리학자 꾸미예요', sender: 'other' },
-    { id: '1', text: { name } + '님의 무의식 속으로 탐험을 떠나볼까요?', sender: 'other' },
+    { id: '1', text: `${name}님의 무의식 속으로 탐험을 떠나볼까요?`, sender: 'other' },
     { id: '2', text: '', sender: 'buttons' }, // 일기장 보기, 상담하기 버튼
   ]);
 
-  const sendMessage = (messageText) => {
+  const sendMessage =  (question) => {
     // 사용자가 메시지를 보냄
-    if (messageText.trim().length > 0) {
-      setMessages([...messages, { id: Date.now().toString(), text: messageText, sender: 'user' }]);
+    if (question.trim().length > 0) {
+      setMessages(prevMessages => [
+        ...prevMessages,
+        { id: Date.now().toString(), text: question, sender: 'user' }
+      ]);
 
-      // 상대방 응답 메세지 생성
-      setTimeout(() => {
-        let responseText = 'writing...'; // 기본 응답
+      const accessToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1IiwiaWF0IjoxNzE0NDg1ODMzLCJleHAiOjE3MTQ0ODk0MzN9.oUPQYOMZGdw8K5ZFT1aHadX_W3A5nAJC4U6CN0SOgzGAWEG3MXJ3U_OHZbh6VXhwrriIV7ySA1kQRF3lLjyQFg';
 
-        if (messageText == '꿈일기 불러오기') {
-          responseText = <BoxComponent />; //꿈일기 목록 불러오기
-        } else if (messageText == '상징의미 파악하기') {
-          // to do 
-        } else if (messageText == '감정 분석하기') {
-          // to do
-        }
+      fetch('http://carvedrem.kro.kr:8080/api/gptChat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ message: question }),
+      })
 
-        // 상대방 응답 메세지 추가
-        setMessages(messages => [...messages, { id: Date.now().toString(), text: responseText, sender: 'other' }]);
-      }, 500); // 응답 지연 시간 설정
-
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.information.answer);
+          // 상대방 응답 메시지 추가
+          setMessages(prevMessages => [
+            ...prevMessages,
+            { id: Date.now().toString(), text: data.information.answer, sender: 'other' }
+          ]);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
     }
-  };
+
+
+
+    // 상대방 응답 메세지 생성
+    // setTimeout(() => {
+    //   let responseText = 'writing...'; // 기본 응답
+
+    //   if (question == '꿈일기 불러오기') {
+    //     responseText = <BoxComponent />; //꿈일기 목록 불러오기
+    //   } else if (question == '상징의미 파악하기') {
+    //     // to do 
+    //   } else if (question == '감정 분석하기') {
+    //     // to do
+    //   }
+
+    //   // 상대방 응답 메세지 추가
+    //   setMessages(messages => [...messages, { id: Date.now().toString(), text: responseText, sender: 'other' }]);
+    // }, 500); // 응답 지연 시간 설정
+
+  }
+
+  // FlatList ref 생성
+  const flatListRef = useRef();
+
+  // FlatList를 자동 스크롤하는 함수
+  useEffect(() => {
+    flatListRef.current.scrollToEnd({ animated: true });
+  }, [messages]);
 
 
   return (
@@ -61,6 +98,7 @@ const ChatView = ({ navigation, name }) => {
 
         {/* 채팅 창 */}
         <FlatList
+          ref={flatListRef}
           data={messages}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => {
@@ -86,13 +124,11 @@ const ChatView = ({ navigation, name }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
+    backgroundColor: '#464E82', // -> 확인
   },
   chatContainer: {
     flex: 1,
     padding: 15,
-    backgroundColor: '#464E82', //남색
-
   },
   dataText: {
     paddingBottom: 10,
