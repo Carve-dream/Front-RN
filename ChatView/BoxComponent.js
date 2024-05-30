baba693f4c9832fd420fe6b6627184c36468723b
Eunkyu Lee <lek7146@gmail.com>
2024-05-31 02:23:57 +0900

    fix bug and get last diaries

--- ChatView/BoxComponent.js
+++ ChatView/BoxComponent.js
@@ -25,27 +25,41 @@ const BoxComponent = () => {
 
     useEffect(() => {
         async function fetchData() {
-            setLoading(true);
             await checkToken();
             token = await getToken();
-        
-            const response = await fetch('http://carvedrem.kro.kr:8080/api/diary?page=0', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token[0]}`,
-                },
-            });
             
-            const ret = await response.json();
+            setLoading(true);
+    
+            let arr = [];
         
-            if (ret.check == null || ret.check == true) {
-                setData(ret.information);
-                console.log("데이터 불러오기 성공");
-                setLoading(false);
-            } else {
-                console.log("데이터 불러오기 실패");
+            for (let index = 0; true; index++) {
+                
+                const response = await fetch('http://carvedrem.kro.kr:8080/api/diary?page=' + index, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token[0]}`,
+                    },
+                });
+                
+                const ret = await response.json();
+            
+                if (ret.check == null || ret.check == true) {
+                    if (ret.information.length == 0) {
+                        break;
+                    }
+                    ret.information.map((element) => {
+                        arr.unshift(element);
+                    })
+                    console.log("데이터 불러오기 성공");
+                } else {
+                    console.log("데이터 불러오기 실패");
+                    break;
+                }
             }
+            console.log(arr);
+            setData(arr);
+            setLoading(false);
         }
         setData([]);
         loadUserData();
@@ -68,12 +82,12 @@ const BoxComponent = () => {
 };
 
 //일기 목록 불러오기 예시
-const List = ({key, title, id}) => {
+const List = ({title, id}) => {
 
     const navigation = useNavigation();
 
     return (
-        <TouchableOpacity key={key} style={styles.listContainer} onPress={() => {navigation.navigate('DiaryDetail', {id: id})}}>
+        <TouchableOpacity style={styles.listContainer} onPress={() => {navigation.navigate('DiaryDetail', {id: id})}}>
             <Text>{title}</Text>
         </TouchableOpacity>
     )
